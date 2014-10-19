e676062c0d94e3b2d174c790c03c15f905129a45
Pete Karl II <pete@upstatement.com>
2014-10-19 09:13:30 -0400

    fixed bug in header setter

--- routes/stories.js
+++ routes/stories.js
@@ -62,7 +62,7 @@ function mapToNiceness(rawStory) {
 /* get ANY story */
 router.get('/random', function(req, res) {
 
-	this.response.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Origin", "*");
 
 	fs.readFile(__dirname + '/../public/data/stories.json', 'utf8', function(err, data) {
 		if (err) { return console.log(err); }
